cddef5df009f32872da017017b2a5103d3aad150
Ngoc Nguyen <nguyenthaingoc162002@gmail.com>
2023-07-03 13:51:27 +0700

    Finish chapter 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,6 @@ class ApplicationController < ActionController::Base
     I18n.with_locale(locale, &action)
   end
 
-  def extract_locale_from_tld
-    parsed_locale = request.host.split(".").last
-    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
-  end
-
   def default_url_options
     {locale: I18n.locale}
   end
