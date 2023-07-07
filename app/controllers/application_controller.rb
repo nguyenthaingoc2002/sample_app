8a22376d16c2368bfa08da75e487738c7f8e6c09
Ngoc Nguyen <nguyenthaingoc162002@gmail.com>
2023-07-07 09:04:45 +0700

    Chapter_3_4_5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  private
+
+  def switch_locale &action
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def extract_locale_from_tld
+    parsed_locale = request.host.split(".").last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
