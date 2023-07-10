3de6b8dedb0892fffe5e919fcd15eba6409c6b65
Ngoc Nguyen <nguyenthaingoc162002@gmail.com>
2023-07-10 13:28:04 +0700

    Finish chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params.dig(:session, :email)&.downcase
+
     if user&.authenticate params[:session][:password]
       log_in user
+      handle_remember user
       redirect_to user
     else
       flash.now[:danger] = t "login.invalid_email_password_combination"
